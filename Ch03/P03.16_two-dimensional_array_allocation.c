21a34bd4037d5838ae8b8b0962e31c972347b284
Piolie <Piolie@users.noreply.github.com>
2022-03-31 14:20:43 -0300

    Add program P16 of Ch03

---
+++ Ch03/P03.16_two-dimensional_array_allocation.c
@@ -0,0 +1,21 @@
+/* ---------------------------------------------------------------------
+        This code is from "Algorithms in C, Third Edition,
+        by Robert Sedgewick, Addison-Wesley, 1998.
+------------------------------------------------------------------------
+             PROGRAM 3.16 Two-dimensional array allocation
+------------------------------------------------------------------------
+This function dynamically allocates the memory for a two-dimensional
+array, as an array of arrays. We first allocate an array of pointers,
+then allocate memory for each row. With this function, the statement
+    int **a = malloc2d(M, N);
+allocates an M-by-N array of integers.
+--------------------------------------------------------------------- */
+
+int **malloc2d(int r, int c)
+{
+    int i;
+    int **t = malloc(r * sizeof(int *));
+    for (i = 0; i < r; i++)
+        t[i] = malloc(c * sizeof(int));
+    return t;
+}
