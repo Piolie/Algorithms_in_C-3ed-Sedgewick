0791b201766552cf98a41ca801a26344288111c1
Piolie <Piolie@users.noreply.github.com>
2022-03-31 15:46:28 -0300

    Add program P19 of Ch03

---
+++ Ch03/P03.19_adjacency-lists_graph_representation.c
@@ -0,0 +1,42 @@
+/* ---------------------------------------------------------------------
+        This code is from "Algorithms in C, Third Edition,
+        by Robert Sedgewick, Addison-Wesley, 1998.
+------------------------------------------------------------------------
+           PROGRAM 3.19 Adjacency-lists graph representation
+------------------------------------------------------------------------
+This program reads a set of edges that define a graph and builds an
+adjacency-matrix representation for the graph. An adjacency list for a
+graph is an array of lists, one for each vertex, where the jth list
+contains a linked list of the nodes connected to the jth vertex.
+--------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node *link;
+struct node
+{
+    int v;
+    link next;
+};
+
+link NEW(int v, link next)
+{
+    link x = malloc(sizeof *x);
+    x->v = v;
+    x->next = next;
+    return x;
+}
+
+int main(void)
+{
+    int i, j;
+    link adj[V];
+    for (i = 0; i < V; i++)
+        adj[i] = NULL;
+    while (scanf("%d %d\n", &i, &j) == 2)
+    {
+        adj[j] = NEW(i, adj[j]) ;
+        adj[i] = NEW(j, adj[i]);
+    }
+}
