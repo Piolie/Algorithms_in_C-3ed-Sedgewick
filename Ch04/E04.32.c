24d93e501e5e6555a246a1ad025142b008232f37
Piolie <Piolie@users.noreply.github.com>
2023-09-11 13:15:15 -0300

    Add solution E04.32

---
+++ Ch04/E04.32.c
@@ -0,0 +1,45 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 4.32
+------------------------------------------------------------------------
+Modify the array-based FIFO queue implementation in the text
+(Program 4.1) to call a function QUEUEerror if the client attempts to
+get when the queue is empty or to put when the queue is full.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+This exercise is analogous to E04.20. I've followed similar strategies.
+--------------------------------------------------------------------- */
+
+#include "Item.h"
+#include <stdlib.h>
+
+static Item *q;
+static int N, head, tail;
+
+void QUEUEinit(int maxN) {
+  q = malloc((maxN + 1) * sizeof(Item));
+  N = maxN + 1;
+  head = N;
+  tail = 0;
+}
+int QUEUEempty(void) { return head % N == tail; }
+
+int QUEUEfull(void) { return tail + 1 == head; }
+
+void QUEUEput(Item item) {
+  if (QUEUEfull())
+    QUEUEerror();
+  else {
+    q[tail++] = item;
+    tail = tail % N;
+  }
+}
+
+Item QUEUEget() {
+  if (QUEUEempty()) {
+    QUEUEerror();
+    return q[0];
+  }
+  head = head % N;
+  return q[head++];
+}
