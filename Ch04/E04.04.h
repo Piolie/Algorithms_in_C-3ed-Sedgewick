9f06bf77a6216548aa2842883df32794ddbc674a
Piolie <Piolie@users.noreply.github.com>
2023-09-14 15:39:34 -0300

    Fix E04.04
    Make Item into a pointer to struct card, fix and test macros

--- Ch04/E04.04.h
+++ Ch04/E04.04.h
@@ -15,39 +15,41 @@ typedef enum { Clubs, Diamonds, Hearts, Spades } SUIT;
 typedef struct card {
   RANK rank;
   SUIT suit;
-} Item;
+} *Item;
 
-#define eq(A, B) (A.RANK == B.RANK && A.SUIT == B.SUIT)
-#define ITEMshow(X)                                                    \
-  switch (X.RANK) {                                                    \
-  case Ace:                                                            \
-    printf("Ace");                                                     \
-    break;                                                             \
-  case Jack:                                                           \
-    printf("Jack");                                                    \
-    break;                                                             \
-  case Queen:                                                          \
-    printf("Queen");                                                   \
-    break;                                                             \
-  case King:                                                           \
-    printf("King");                                                    \
-    break;                                                             \
-  default:                                                             \
-    printf("%d", X.RANK);                                              \
-    break;                                                             \
-  };                                                                   \
-  printf(" of ");                                                      \
-  switch (X.SUIT) {                                                    \
-  case Clubs:                                                          \
-    printf("Clubs");                                                   \
-    break;                                                             \
-  case Diamonds:                                                       \
-    printf("Diamonds");                                                \
-    break;                                                             \
-  case Hearts:                                                         \
-    printf("Hearts");                                                  \
-    break;                                                             \
-  case Spades:                                                         \
-    printf("Spades");                                                  \
-    break;                                                             \
+#define eq(A, B) ((A)->rank == (B)->rank && (A)->suit == (B)->suit)
+#define ITEMshow(X)                                                            \
+  {                                                                            \
+    switch (X->rank) {                                                         \
+    case Ace:;                                                                 \
+      printf("Ace");                                                           \
+      break;                                                                   \
+    case Jack:                                                                 \
+      printf("Jack");                                                          \
+      break;                                                                   \
+    case Queen:                                                                \
+      printf("Queen");                                                         \
+      break;                                                                   \
+    case King:                                                                 \
+      printf("King");                                                          \
+      break;                                                                   \
+    default:                                                                   \
+      printf("%d", X->rank);                                                   \
+      break;                                                                   \
+    };                                                                         \
+    printf(" of ");                                                            \
+    switch (X->suit) {                                                         \
+    case Clubs:                                                                \
+      printf("Clubs");                                                         \
+      break;                                                                   \
+    case Diamonds:                                                             \
+      printf("Diamonds");                                                      \
+      break;                                                                   \
+    case Hearts:                                                               \
+      printf("Hearts");                                                        \
+      break;                                                                   \
+    case Spades:                                                               \
+      printf("Spades");                                                        \
+      break;                                                                   \
+    }                                                                          \
   }
