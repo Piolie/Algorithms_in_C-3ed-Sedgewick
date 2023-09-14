bcbdb87046a183714238cd87c7c5dc2550024775
Piolie <Piolie@users.noreply.github.com>
2023-09-14 17:39:42 -0300

    Add solution E04.46

---
+++ Ch04/E04.46.c
@@ -0,0 +1,11 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 4.46
+------------------------------------------------------------------------
+Write a program that solves the connectivity problem by inserting all
+the pairs on a random queue and then taking them from the queue, using
+the quick-find-weighted algorithm (Program 1.3).
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+This exercise doesn't add anything to the learning process.
+--------------------------------------------------------------------- */
