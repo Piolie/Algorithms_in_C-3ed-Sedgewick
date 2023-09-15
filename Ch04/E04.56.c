96f5336d147795bd19edd99a2fcc9d8871c61594
Piolie <Piolie@users.noreply.github.com>
2023-09-15 18:10:38 -0300

    Add solution E04.56

---
+++ Ch04/E04.56.c
@@ -0,0 +1,12 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 4.57
+------------------------------------------------------------------------
+Write a client program for your ADT from Exercise 4.55, which exercises
+a randomized queue with no duplicates.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+Doesn't add much. See clients implemented for debugging other exercises,
+i.e. E04.54. Since the ADT is the same, we don't have to change
+anything.
+--------------------------------------------------------------------- */
