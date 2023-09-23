a4850d40efc957f0e7ed2e92611e9b20cbeedd07
Piolie <Piolie@users.noreply.github.com>
2023-09-23 10:37:08 -0300

    Add solution E04.78

---
+++ Ch04/E04.78.c
@@ -0,0 +1,19 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 4.78
+------------------------------------------------------------------------
+Write a client program that uses your polynomial ADT from Exercise 4.75
+to evaluate integrals by using Taylor series approximations of
+functions, manipulating them symbolically.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+Accomplishing this requires that we know (or can compute) values of the
+function of interest and of some of its derivatives around a certain
+point. Once we have that, we can construct its Taylor series
+approximation and integrate the series to calculate the integal.
+For the last part, the functions provided by the polynomial ADT are
+enough.
+
+Reference: [Ma 8: Problem Solving in Calculus, week
+7](http://math.caltech.edu/~syye/teaching/courses/Ma8_2015/Lecture%20Notes/ma8_wk7.pdf)
+--------------------------------------------------------------------- */
