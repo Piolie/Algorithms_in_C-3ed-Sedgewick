f77b6460fe976cfd66e5f45056d785fcc453da19
Piolie <Piolie@users.noreply.github.com>
2023-09-28 15:41:14 -0300

    Fix output format in E05.08

--- Ch05/E05.08.c
+++ Ch05/E05.08.c
@@ -9,19 +9,19 @@ prefix-expression evaluation for the input + * * 12 12 12 144.
 
 static char *a;
 static int i = 0;
-static int depth = 0;
 
 int eval(void) {
+  static int depth = -1;
   depth++;
   int x = 0;
   while (a[i] == ' ')
     i++;
-  printf("%*seval() %s\n", depth, "", &a[i]);
+  printf("%*seval() %s\n", 2 * depth, "", &a[i]);
   if (a[i] == '+') {
     i++;
     int e1 = eval();
     int e2 = eval();
-    printf("%*sreturn %d = %d + %d\n", depth + 1, "", e1 + e2, e1, e2);
+    printf("%*sreturn %d = %d + %d\n", 2 * (depth + 1), "", e1 + e2, e1, e2);
     depth--;
     return e1 + e2;
   }
@@ -29,7 +29,7 @@ int eval(void) {
     i++;
     int e1 = eval();
     int e2 = eval();
-    printf("%*sreturn %d = %d * %d\n", depth + 1, "", e1 * e2, e1, e2);
+    printf("%*sreturn %d = %d * %d\n", 2 * (depth + 1), "", e1 * e2, e1, e2);
     depth--;
     return e1 * e2;
   }
@@ -50,32 +50,32 @@ int main(int argc, char *argv[]) {
 ------------------------------------------------------------------------
 Reproducing figure 5.3:
 ./E05.08.exe "* + 7 * * 4 6 + 8 9 5"
- eval() * + 7 * * 4 6 + 8 9 5
+eval() * + 7 * * 4 6 + 8 9 5
   eval() + 7 * * 4 6 + 8 9 5
-   eval() 7 * * 4 6 + 8 9 5
-   eval() * * 4 6 + 8 9 5
-    eval() * 4 6 + 8 9 5
-     eval() 4 6 + 8 9 5
-     eval() 6 + 8 9 5
-     return 24 = 4 * 6
-    eval() + 8 9 5
-     eval() 8 9 5
-     eval() 9 5
-     return 17 = 8 + 9
-    return 408 = 24 * 17
-   return 415 = 7 + 408
+    eval() 7 * * 4 6 + 8 9 5
+    eval() * * 4 6 + 8 9 5
+      eval() * 4 6 + 8 9 5
+        eval() 4 6 + 8 9 5
+        eval() 6 + 8 9 5
+        return 24 = 4 * 6
+      eval() + 8 9 5
+        eval() 8 9 5
+        eval() 9 5
+        return 17 = 8 + 9
+      return 408 = 24 * 17
+    return 415 = 7 + 408
   eval() 5
   return 2075 = 415 * 5
 
 ./E05.08.exe "+ * * 12 12 12 144"
- eval() + * * 12 12 12 144
+eval() + * * 12 12 12 144
   eval() * * 12 12 12 144
-   eval() * 12 12 12 144
-    eval() 12 12 12 144
-    eval() 12 12 144
-    return 144 = 12 * 12
-   eval() 12 144
-   return 1728 = 144 * 12
+    eval() * 12 12 12 144
+      eval() 12 12 12 144
+      eval() 12 12 144
+      return 144 = 12 * 12
+    eval() 12 144
+    return 1728 = 144 * 12
   eval() 144
   return 1872 = 1728 + 144
 --------------------------------------------------------------------- */
