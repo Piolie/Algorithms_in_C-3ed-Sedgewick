737ed111201711c54103980d41953536dd9aa4ce
Piolie <Piolie@users.noreply.github.com>
2023-09-28 15:39:24 -0300

    Fix output format in E05.03

--- Ch05/E05.03.c
+++ Ch05/E05.03.c
@@ -12,7 +12,7 @@ invoked for each of the integers 1 through 9.
 static int depth;
 
 int puzzle(int N) {
-  printf("%*spuzzle(%d)\n", depth, "", N);
+  printf("%*spuzzle(%d)\n", 2 * depth, "", N);
   depth++;
   if (N == 1)
     return 1;
@@ -34,92 +34,92 @@ int main(void) {
 /* ---------------------------------------------------------------------
                                  OUTPUT
 ------------------------------------------------------------------------
-./E05.03.exe
+./E05.03.exe./E05.03.exe
 N = 1
 puzzle(1)
 
 N = 2
 puzzle(2)
- puzzle(1)
+  puzzle(1)
 
 N = 3
 puzzle(3)
- puzzle(10)
-  puzzle(5)
-   puzzle(16)
-    puzzle(8)
-     puzzle(4)
-      puzzle(2)
-       puzzle(1)
+  puzzle(10)
+    puzzle(5)
+      puzzle(16)
+        puzzle(8)
+          puzzle(4)
+            puzzle(2)
+              puzzle(1)
 
 N = 4
 puzzle(4)
- puzzle(2)
-  puzzle(1)
+  puzzle(2)
+    puzzle(1)
 
 N = 5
 puzzle(5)
- puzzle(16)
-  puzzle(8)
-   puzzle(4)
-    puzzle(2)
-     puzzle(1)
+  puzzle(16)
+    puzzle(8)
+      puzzle(4)
+        puzzle(2)
+          puzzle(1)
 
 N = 6
 puzzle(6)
- puzzle(3)
-  puzzle(10)
-   puzzle(5)
-    puzzle(16)
-     puzzle(8)
-      puzzle(4)
-       puzzle(2)
-        puzzle(1)
+  puzzle(3)
+    puzzle(10)
+      puzzle(5)
+        puzzle(16)
+          puzzle(8)
+            puzzle(4)
+              puzzle(2)
+                puzzle(1)
 
 N = 7
 puzzle(7)
- puzzle(22)
-  puzzle(11)
-   puzzle(34)
-    puzzle(17)
-     puzzle(52)
-      puzzle(26)
-       puzzle(13)
-        puzzle(40)
-         puzzle(20)
-          puzzle(10)
-           puzzle(5)
-            puzzle(16)
-             puzzle(8)
-              puzzle(4)
-               puzzle(2)
-                puzzle(1)
+  puzzle(22)
+    puzzle(11)
+      puzzle(34)
+        puzzle(17)
+          puzzle(52)
+            puzzle(26)
+              puzzle(13)
+                puzzle(40)
+                  puzzle(20)
+                    puzzle(10)
+                      puzzle(5)
+                        puzzle(16)
+                          puzzle(8)
+                            puzzle(4)
+                              puzzle(2)
+                                puzzle(1)
 
 N = 8
 puzzle(8)
- puzzle(4)
-  puzzle(2)
-   puzzle(1)
+  puzzle(4)
+    puzzle(2)
+      puzzle(1)
 
 N = 9
 puzzle(9)
- puzzle(28)
-  puzzle(14)
-   puzzle(7)
-    puzzle(22)
-     puzzle(11)
-      puzzle(34)
-       puzzle(17)
-        puzzle(52)
-         puzzle(26)
-          puzzle(13)
-           puzzle(40)
-            puzzle(20)
-             puzzle(10)
-              puzzle(5)
-               puzzle(16)
-                puzzle(8)
-                 puzzle(4)
-                  puzzle(2)
-                   puzzle(1)
+  puzzle(28)
+    puzzle(14)
+      puzzle(7)
+        puzzle(22)
+          puzzle(11)
+            puzzle(34)
+              puzzle(17)
+                puzzle(52)
+                  puzzle(26)
+                    puzzle(13)
+                      puzzle(40)
+                        puzzle(20)
+                          puzzle(10)
+                            puzzle(5)
+                              puzzle(16)
+                                puzzle(8)
+                                  puzzle(4)
+                                    puzzle(2)
+                                      puzzle(1)
 --------------------------------------------------------------------- */
