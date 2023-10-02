a9bdf2975a0b2c001e85bacbafeae586dfb719fe
Piolie <Piolie@users.noreply.github.com>
2023-10-02 10:42:53 -0300

    Add MISSING E05.25

---
+++ Ch05/E05.25.c
@@ -0,0 +1,20 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 5.25
+------------------------------------------------------------------------
+Write a recursive program that fills in an n-by-2^n array with 0s and 1s
+such that the array represents all the n-bit binary numbers, as depicted
+in Figure 5.9.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+Figure 5.9 shows an array of 2^n-by-n.
+
+The book suggests the following procedure:
+  Print the table of (n - 1)-bit numbers, each preceded by a 0 bit, then
+  print the table of (n - 1)-bit numbers each preceded by a 1-bit (see
+  Exercise 5.25).
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+MISSING!
+--------------------------------------------------------------------- */
