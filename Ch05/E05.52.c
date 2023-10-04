f0310d91d82aed41a8e7f2ed752ac3eff24e37bd
Piolie <Piolie@users.noreply.github.com>
2023-10-04 17:08:58 -0300

    Add MISSING E05.52

---
+++ Ch05/E05.52.c
@@ -0,0 +1,13 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 5.52
+------------------------------------------------------------------------
+Write a function that solves the knapsack problem using top-down dynamic
+programming, but using a recursive solution based on computing the
+optimal number of a particular item to include in the knapsack, based on
+(recursively) knowing the optimal way to pack the knapsack without that
+item.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+MISSING!
+--------------------------------------------------------------------- */
