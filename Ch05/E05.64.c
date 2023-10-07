fe790df437eb4bcfd4a017ea1fd39ddd8401a866
Piolie <Piolie@users.noreply.github.com>
2023-10-07 15:14:56 -0300

    Add MISSING E05.61-E05.67

---
+++ Ch05/E05.64.c
@@ -0,0 +1,15 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 5.64
+------------------------------------------------------------------------
+Write a program to determine whether or not two arrays of N integers
+between 0 and N - 1 represent isomorphic unordered trees, when
+interpreted (as in Chapter 1) as parent-child links in a tree with nodes
+numbered between 0 and N - 1. That is, your program should determine
+whether or not there is away to renumber the nodes in one tree such that
+the array representation of the one tree is identical to the array
+representation of the other tree.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+MISSING!
+--------------------------------------------------------------------- */
