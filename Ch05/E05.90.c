8e96646392dd6ecd9e2baddf2b7febcd5f74bd2a
Piolie <Piolie@users.noreply.github.com>
2023-10-13 12:36:54 -0300

    Add solution E05.90

---
+++ Ch05/E05.90.c
@@ -0,0 +1,13 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 5.90
+------------------------------------------------------------------------
+Write a recursive program that computes the internal path length of a
+binary tree in time proportional to the number of nodes in the tree.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+If my results from E05.89 are not wrong, then that solution already
+computes the internal path length in time proportional to the number of
+nodes in the tree. I don't get what the difference between these two
+programs should be.
+--------------------------------------------------------------------- */
