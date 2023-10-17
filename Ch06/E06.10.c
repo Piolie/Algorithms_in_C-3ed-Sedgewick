dc9acabd79d473648b4b7cbc8a69199ce2f0faf3
Piolie <Piolie@users.noreply.github.com>
2023-10-17 11:56:10 -0300

    Add MISSING E06.10

---
+++ Ch06/E06.10.c
@@ -0,0 +1,13 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 6.10
+------------------------------------------------------------------------
+Write an exercise driver client program that runs sort on difficult or
+pathological cases that might turn up in practical applications.
+Examples include files that are already in order, files in reverse
+order, files where all keys are the same, files consisting of only two
+distinct values, and files of size 0 or 1.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+MISSING!
+--------------------------------------------------------------------- */
