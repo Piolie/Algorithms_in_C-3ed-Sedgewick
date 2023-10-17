62f25f8609c469ceae9352972e2e9ec1b287f867
Piolie <Piolie@users.noreply.github.com>
2023-10-17 11:56:07 -0300

    Add MISSING E06.09

---
+++ Ch06/E06.09.c
@@ -0,0 +1,11 @@
+/* ---------------------------------------------------------------------
+                              EXERCISE 6.9
+------------------------------------------------------------------------
+Write a performance driver client program that runs sort multiple times
+on files of various sizes, measures the time taken for each run, and
+prints out or plots the average running times.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+MISSING!
+--------------------------------------------------------------------- */
