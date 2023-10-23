b729b439d0e17ca9bb9b10097c4f4e49575f84bf
Piolie <Piolie@users.noreply.github.com>
2023-10-23 17:47:23 -0300

    Add MISSING E06.56-E06.62

---
+++ Ch06/E06.56.c
@@ -0,0 +1,12 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 6.56
+------------------------------------------------------------------------
+Give an implementation of a data type for items where the items are
+records, rather than pointers to records. This arrangement might be
+preferable to Programs 6.12 and 6.13 for small records. (Remember that
+C supports structure assignment.)
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+MISSING!
+--------------------------------------------------------------------- */
