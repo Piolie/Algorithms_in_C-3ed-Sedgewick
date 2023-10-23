423a87033470328ef9968aa132c32fddc54b236e
Piolie <Piolie@users.noreply.github.com>
2023-10-23 16:57:05 -0300

    Add MISSING E06.53-E06.55

---
+++ Ch06/E06.53.c
@@ -0,0 +1,12 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 6.53
+------------------------------------------------------------------------
+Add a function testinit to the array data type in Programs 6.8 and 6.7,
+which generates test data according to distributions similar to those
+illustrated in Figure 6.13. Provide an integer argument for the client
+to use to specify the distribution.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+MISSING!
+--------------------------------------------------------------------- */
---
+++ Ch06/E06.55.c
@@ -0,0 +1,13 @@
+/* ---------------------------------------------------------------------
+                             EXERCISE 6.55
+------------------------------------------------------------------------
+Write an interface and implementation for the generic item data type for
+use in having the sorting methods sort multidimensional vectors of d
+integers, putting the vectors in order by first component, those with
+equal first component in order by second component, those with equal
+first and second components in order by third component, and so forth.
+--------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------
+MISSING!
+--------------------------------------------------------------------- */
